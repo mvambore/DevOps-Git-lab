cd596c50e3647f91b67bcbc1ac636cdcf138c8ee
mva_lab <mva@y.com>
2021-10-28 09:35:25 +0530

    DevOps labs 2 assn1

---
+++ assn1.1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+	int a = 30, b = 10;
+	
+	printf("%d\n", (a + b));
+}
---
+++ assn1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+	int a = 32;
+	printf("Hello from MSIS DevOps lab2.");
+}
